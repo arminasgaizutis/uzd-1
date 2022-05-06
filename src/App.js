9a53cd5f88c2a101389266a1f9263f69cabc7395
Arminas Gaizutis <arminkeef@gmail.com>
2022-05-06 07:57:42 +0300

    app js atvaizdavimas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Block from './components/Block/Block';
+import Button from './components/Button/Button';
+import Hero from './components/Hero/Hero';
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <>
+      <Hero title='Success hero' subtitle='Success subtitle' />
+      <Hero color='danger' title='A' subtitle='B' />
+
+      <Block color='primary'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus,
+        voluptatibus.
+      </Block>
+      <Block color='success'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam,
+        quibusdam!
+      </Block>
+      <Block color='danger'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        obcaecati?
+      </Block>
+
+      <Button color='primary'>1</Button>
+      <Button color='success'>2</Button>
+      <Button color='danger'>3</Button>
+    </>
+  );
+}
 
 export default App;
